53b446458a5408d72bfdca435914892fac0be1e4
ajzal-byte <ajzalaju10@gmail.com>
2025-03-29 15:52:34 +0530

    update: config updated and logs deleted

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 // Configurations
 define('AUTH_TOKEN', "226721beeb20dadc5fa2a89d4e3d383c");
-// define('TIMESTAMP', date("Y-m-d H:i:s")); // Production
-define('TIMESTAMP', "2025-03-23 00:00:00"); // Testing
+define('TIMESTAMP', date("Y-m-d H:i:s")); // Production
+// define('TIMESTAMP', "2025-03-23 00:00:00"); // Testing
 define('LEAD_FILE', __DIR__ . '/processed_leads.txt');
 define('DEFAULT_ASSIGNED_USER_ID', '16'); // Driton Puka
 define('SECONDARY_PIPELINE_ID', 24);
